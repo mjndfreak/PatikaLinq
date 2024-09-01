21f66e9f6f0afc4641d5023216b1de9ad3f3fb40
mjndfreak <cagdasergencc@gmail.com>
2024-09-01 21:00:37 +0300

    initial commit

---
+++ PatikLinq/PatikLinq/Program.cs
@@ -0,0 +1,60 @@
+﻿Random rnd = new Random();
+List<int> randomNumbers = new List<int>();
+
+for (int i = 0; i < 10; i++)
+{
+    int random = rnd.Next(-1000, 1001);
+    randomNumbers.Add(random);
+}
+
+var evenNumbers = randomNumbers.Where(n => n % 2 == 0);
+
+foreach (var number in evenNumbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("-------------------");
+
+var oddNumbers = randomNumbers.Where(n => n % 2 == 1);
+
+foreach (var number in oddNumbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("-------------------");
+
+var negativeNumbers = randomNumbers.Where(n => n < 0);
+
+foreach (var number in negativeNumbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("-------------------");
+
+var positiveNumbers = randomNumbers.Where(n => n > 0);
+
+foreach (var number in positiveNumbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("-------------------");
+
+var btw = randomNumbers.Where(n => n < 22 && n > -538);
+
+foreach (var number in btw)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("-------------------");
+
+var squares = randomNumbers.Select(n => n * n).ToList();
+
+foreach (var number in squares)
+{
+    Console.WriteLine(number);
+}
